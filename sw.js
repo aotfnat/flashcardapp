0c78532a8a12119bf57a6a8d2867fe749fd07dd2
aotfnat <aotfnat@yahoo.com>
2026-04-14 15:37:57 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+// sw.js - Service Worker for offline support
+const CACHE_NAME = 'flashcards-v1';
+const urlsToCache = [
+  './',                    // index.html
+  'index.html',
+  'manifest.json'
+  // Add any other important files here (CSS, images, etc.)
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
